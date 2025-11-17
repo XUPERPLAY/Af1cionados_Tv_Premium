a99bade8576941a43eae778d18ab02fd22a9ce37
XUPERPLAY <tintxicasa2@gmail.com>
2025-11-17 23:28:01 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,18 +1,22 @@
 // api/proxy.js
-export const config = { api: { bodyParser: false } };
-
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export default createProxyMiddleware({
   target: 'https://dlhd.dad',
   changeOrigin: true,
   pathRewrite: {
-    '^/api/proxy': '',
+    '^/api/proxy': '/stream',
   },
-  onProxyRes: (proxyRes, req, res) => {
-    // Eliminamos headers que causan problemas
+  onProxyRes: (proxyRes) => {
     delete proxyRes.headers['x-frame-options'];
     delete proxyRes.headers['content-security-policy'];
     delete proxyRes.headers['frame-options'];
   },
 });
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+};
